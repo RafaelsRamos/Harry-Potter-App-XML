0d13fde67b43052243720cb7875a78ae34ab2a45
Rafael Ramos <rafaelsramos95@gmail.com>
2023-07-24 20:14:13 +0100

    Create listing fragment

---
+++ feature/characterlisting/src/main/java/com/scookie/feature/characterlisting/CharacterListAdapter.kt
@@ -0,0 +1,78 @@
+package com.scookie.feature.characterlisting
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.scookie.domainmodel.Character
+import com.scookie.feature.characterlisting.CharacterListAdapter.ViewHolder
+
+class CharacterListAdapter(
+    private val characters: MutableList<Character>,
+    private val onCharacterClick: (String) -> Unit
+): RecyclerView.Adapter<ViewHolder>() {
+
+    override fun getItemCount() = characters.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.component_character, null)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(characters[position])
+    }
+
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+        private val characterImageView: ImageView = view.findViewById(R.id.character_iv)
+        private val nameTextView: TextView = view.findViewById(R.id.name_tv)
+        private val aliasTextView: TextView = view.findViewById(R.id.alias_tv)
+        fun bind(character: Character) {
+
+            itemView.setOnClickListener {
+                onCharacterClick.invoke(character.id)
+            }
+
+            characterImageView.loadImageFrom(character.image)
+
+            nameTextView.text = character.name
+
+            val alias = character.alternateNames.firstOrNull()
+
+            if (alias != null) {
+                showAlias(alias)
+            } else {
+                hideAlias()
+            }
+        }
+
+        private fun showAlias(alias: String) {
+            aliasTextView.text = alias
+            aliasTextView.visibility = View.VISIBLE
+        }
+
+        private fun hideAlias() {
+            aliasTextView.visibility = View.GONE
+        }
+
+        private fun ImageView.loadImageFrom(url: String) {
+            Glide.with(context)
+                .load(url)
+                .transition(DrawableTransitionOptions.withCrossFade(200))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(R.drawable.character_placeholder)
+                .into(this)
+
+        }
+
+    }
+
+}
---
+++ feature/characterlisting/src/main/java/com/scookie/feature/characterlisting/CharacterListingFragment.kt
@@ -0,0 +1,67 @@
+package com.scookie.feature.characterlisting
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.scookie.data.repositories.characters.CharactersRepository
+import com.scookie.domainmodel.Character
+import com.scookie.navigation.Navigator
+import com.scookie.navigation.screens.CharacterScreen
+import com.scookie.ui.GridSpacingItemDecoration
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class CharacterListingFragment: Fragment(R.layout.screen_character_listing) {
+
+    @Inject
+    lateinit var navigator: Navigator
+
+    @Inject
+    lateinit var characterRepository: CharactersRepository
+
+    private val recyclerView: RecyclerView by lazy { requireView().findViewById(R.id.characters_rv) }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Toast.makeText(context, "I'm hereeeee", Toast.LENGTH_SHORT).show()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            getAllCharacters()
+        }
+    }
+
+    private suspend fun getAllCharacters() {
+        val characters = characterRepository.getAllCharacters()
+
+        withContext(Dispatchers.Main) {
+            recyclerView.setup(characters)
+        }
+    }
+
+    private fun RecyclerView.setup(characters: List<Character>) {
+        adapter = CharacterListAdapter(characters.toMutableList(), ::onCharacterClick)
+        layoutManager = GridLayoutManager(context, GRID_SPAN)
+        addItemDecoration(GridSpacingItemDecoration(GRID_SPAN, 45))
+    }
+
+    private fun onCharacterClick(characterId: String) {
+        navigator.navigateTo(CharacterScreen(characterId))
+    }
+
+    companion object {
+
+        private const val GRID_SPAN = 2
+
+        fun newInstance() = CharacterListingFragment()
+
+    }
+
+}
